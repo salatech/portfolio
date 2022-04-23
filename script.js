2faf0d309d9675b747238c2c463d80805e39f3ab
sala-design <solahudeenbabatunde@gmail.com>
2022-04-23 14:43:38 +0100

    commit\

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Coding Tutor", "Freelancer", "Content Writer"],
+        strings: ["Developer", "Coding Tutor", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Coding Tutor", "Freelancer", "Content Writer"],
+        strings: ["Developer", "Coding Tutor", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
